4eb45cfa42422c3ce378ec7a258d3f09b5c9f31c
Michael Friis <friism@gmail.com>
2013-07-10 21:16:59 -0700

    initial commit

---
+++ KatanaTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace KatanaTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var port = 8080;
+			if (args.Length > 0)
+			{
+				int.TryParse(args[0], out port);
+			}
+
+			using (WebApp.Start<Startup>(port))
+			{
+				Console.WriteLine("Started");
+				Console.ReadKey();
+				Console.WriteLine("Stopping");
+			}
+		}
+	}
+}
